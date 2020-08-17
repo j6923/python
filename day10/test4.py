118501122b36bd731d246a1afd5455e0190095d7
j6923 <69246174+j6923@users.noreply.github.com>
2020-08-18 08:33:09 +0900

    Update test4.py

--- day10/test4.py
+++ day10/test4.py
@@ -25,21 +25,21 @@ d.status()  #d의 status 호출
 #     def __init__(self):  #초기화함
 #         print("초기화함수") #초기화함수를 호출
 #         self.company = '현대' #class car의 company석성을 현대로 함
-#         self.name = 'Sonata'
-#         self.color = 'red'
-#         self.year = '1년'
-#         self.wheel = 1
-#         self.handle = 1
-#     def forward(self):
-#         print("전진하자")
-#     def back(self):
-#         print("후진")
-#     def light(self):
-#         print("깜박깜박")
-#     def fast(self):
-#         print("가속")
-#     def slow(self):
-#         print("감속")
+#         self.name = 'Sonata' #속성 name을 Sonata로 함
+#         self.color = 'red' #속성 color을 red로 함.
+#         self.year = '1년' #속성 year을 1년으로 함
+#         self.wheel = 1 #속성 wheel을 1로 함
+#         self.handle = 1  #속성 handle을 1로 함.
+#     def forward(self):  #동작 forward를 함수로 만듦
+#         print("전진하자")  #함수가 호출되면 전진하자 출력
+#     def back(self): #함수 back을 만듦
+#         print("후진") #함수가 호출되면 후진출력
+#     def light(self): #동작 light를 함수로 만듦
+#         print("깜박깜박") #함수가 호출되면 깜박깜박출력
+#     def fast(self): #동작을 fast라는 함수로 만듦.
+#         print("가속") #함수가 호출되면 가속 출력
+#     def slow(self): #동작 
+#         print("감속") #함수가 호출되면 
 #     def stop(self):
 #         print("정지")
 
