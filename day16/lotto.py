e01a583059405a11d94a583b40d7e4632636c95f
j6923 <69246174+j6923@users.noreply.github.com>
2020-08-24 07:55:20 +0900

    Update lotto.py

--- day16/lotto.py
+++ day16/lotto.py
@@ -1,25 +1,38 @@
-import sys
+import sys  #sys를 불러온다. #sys는 시스템
 from PyQt5.QtWidgets import *
+#PyQt5의 QtWidgets에서 전체를 불러온다.
 from PyQt5.QtGui import * 
+#PyQt5의 QtGui에서 전체를 불러온다.
 from PyQt5.QtCore import *
+#pyQt5의 QtCore에서 전체를 불러온다.
 import random 
+#램덤을 불러온다.
 
-class lotto(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+class lotto(QWidget): lotto클래스를 만듦
+    def __init__(self):  #초기화함
+        super().__init__() #부모의 것을 상속받음
+        self.initUI() UI를 만들거야
 
-    def initUI(self):
+    def initUI(self):  initUI라는 함수를 만듦
         self.setGeometry(600,600,800,800) 
+         #크기를 지정(x y 너비 높이)
+         #x값 600 y 값 600 너비 800 높이 800으로 지정
         self.setWindowTitle("인생은 한방!")
+        # 창틀을 인생은 한방이라고 적음
 
         #라벨을 만든다
         self.label1 = QLabel("ball",self)
+        #라벨을 만들어 ball이라고 쓰고 label1에 대입
         self.label2 = QLabel("ball",self)
+        #라벨을 만들어 ball이라고 쓰고 label2에 대입
         self.label3 = QLabel("ball",self)
+        라벨을 만들어 ball이라고 쓰고 label3에 대입
         self.label4 = QLabel("ball",self)
+        라벨을 만들어 ball이라고 쓰고 label4에 대입
         self.label5 = QLabel("ball",self)
+        라벨을 만들어 ball 이라고 쓰고 label5에 대입
         self.label6 = QLabel("ball",self)
+        라벨을 만들어 ball이라고 쓰고 label6에 대입
 
         #라벨에 기본이미지를 변경해준다
         self.label1.setPixmap(QPixmap("./img/lotto/q.jpg"))
